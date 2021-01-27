9fbd92e0e627571f83a3bf7fbc5b97848235c064
Jay Hastings <jayhjman@gmail.com>
2021-01-27 10:47:13 -0800

    removed other maps

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -125,20 +125,6 @@ function init() {
   });
 
   // Create base layers
-  // Satellite Layer
-  var satelliteMap = L.tileLayer(
-    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-    {
-      attribution:
-        "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-      tileSize: 512,
-      maxZoom: 18,
-      zoomOffset: -1,
-      id: "mapbox/satellite-v9",
-      accessToken: API_KEY,
-    }
-  );
-
   // Grayscale Layer
   var grayscaleMap = L.tileLayer(
     "https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
@@ -151,23 +137,9 @@ function init() {
     }
   );
 
-  // Outdoors Layer
-  var outdoorsMap = L.tileLayer(
-    "https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-    {
-      attribution:
-        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      maxZoom: 18,
-      id: "outdoors-v11",
-      accessToken: API_KEY,
-    }
-  );
-
   // Define a baseMaps object to hold our base layers
   var baseMaps = {
-    Satellite: satelliteMap,
     Grayscale: grayscaleMap,
-    Outdoors: outdoorsMap,
   };
 
   // Create overlay object to hold our overlay layer
@@ -179,7 +151,7 @@ function init() {
   var myMap = L.map("mapid", {
     center: [28.0, -35.0],
     zoom: 3,
-    layers: [satelliteMap, earthquakes],
+    layers: [grayscaleMap, earthquakes],
   });
 
   // Place the legend control
